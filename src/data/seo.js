ba31a7eeb2f260fcff88e1947de3df6939a50a89
KamFretoZ <14798312+kamfretoz@users.noreply.github.com>
2025-03-21 21:41:54 +0700

    Initial commit

---
+++ src/data/seo.js
@@ -0,0 +1,31 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"I am a passionate Frontend Web Developer with a strong eye for design and user experience. I specialize in building responsive, dynamic, and intuitive web applications using modern technologies such as HTML, CSS, JavaScript, and frameworks like React.",
+		keywords: ["Ilham", "Ilham Ibrahim"],
+	},
+
+	{
+		page: "about",
+		description:
+			"I am a passionate Frontend Web Developer with a strong eye for design and user experience. I specialize in building responsive, dynamic, and intuitive web applications using modern technologies such as HTML, CSS, JavaScript, and frameworks like React.",
+		keywords: ["Ilham", "Ilham Ibrahim"],
+	},
+
+	{
+		page: "projects",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
+		keywords: ["Ilham", "Ilham Ibrahim"],
+	},
+
+	{
+		page: "contact",
+		description:
+			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
+		keywords: ["Ilham", "Ilham Ibrahim"],
+	},
+];
+
+export default SEO;
---
+++ src/data/user.js
@@ -0,0 +1,38 @@
+const INFO = {
+	main: {
+		title: "Ilham Ibrahim",
+		name: "Ilham Ibrahim",
+		email: "iiibrahim89@gmail.com",
+		logo: "../logo.gif",
+	},
+
+	socials: {
+		github: "https://github.com/kamfretoz/",
+		linkedin: "https://www.linkedin.com/in/ilhamibrahim/",
+	},
+
+	homepage: {
+		title: "Front-end web developer.",
+		description:
+			"Detail-oriented professional with a Bachelor's degree in Mass Communication from BINUS University and a strong technical foundation in computer networks. Skilled in computer software and hardware, proficient in setting up operating systems (Windows and Linux), and experienced in utilizing networking tools (Cisco Packet Tracer, GNS3, and Bind9). Possess one year of industry experience in maintaining IT infrastructure, including configuring Wi-Fi networks, switches, routers, and assigning static IPs.",
+	},
+
+	about: {
+		title: "Hi! I'm Ilham Ibrahim.",
+		description:
+			"I am a passionate Frontend Web Developer with a strong eye for design and user experience. I specialize in building responsive, dynamic, and intuitive web applications using modern technologies such as HTML, CSS, JavaScript, and frameworks like React.",
+	},
+
+	projects: [
+		{
+			title: "TAIG4",
+			description:
+				"TAIG4 (Trained AI Gen. 4) is a trash-talking chess bot that can beat any human player in existence.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/kamfretoz/ChessJS",
+		},
+	],
+};
+
+export default INFO;
